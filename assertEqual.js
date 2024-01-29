f3b1ee352cd87e508045727d54046d8c959755a1
Daniel Minogue <danielcminogue@gmail.com>
2024-01-28 20:54:59 -0700

    Updated  assertEqual js file

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed");
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🔴🔴🔴 Assertion Failed");
+    console.log(`🔴🔴🔴 Assertion Failed`);
   }
 
 
