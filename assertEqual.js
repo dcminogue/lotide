bfc4ae87fe0fa55c458d9af17c3bb2f6588b737a
Daniel Minogue <danielcminogue@gmail.com>
2024-01-28 20:37:28 -0700

    Created assetEqual js file

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed");
+  } else if (actual !== expected) {
+    console.log("🔴🔴🔴 Assertion Failed");
+  }
+
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Roy", "Jamie");
+assertEqual("Jamie, I'm me", "Jamie, I'm me");
+assertEqual(7, 7);
+assertEqual(12, 15);
