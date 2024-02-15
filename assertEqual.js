916b5b361ccca563ae933c7464e045a8745f5953
Daniel Minogue <danielcminogue@gmail.com>
2024-02-15 14:14:10 -0700

    Updated assertEqual and head and added test folder

--- assertEqual.js
+++ assertEqual.js
@@ -5,13 +5,8 @@ const assertEqual = function(actual, expected) {
     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 
-
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Roy", "Jamie");
-assertEqual("Jamie, I'm me", "Jamie, I'm me");
-assertEqual(7, 7);
-assertEqual(12, 15);
+module.exports = assertEqual;
+
+
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require(`../assertEqual`);
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Roy", "Jamie");
+assertEqual("Jamie, I'm me", "Jamie, I'm me");
+assertEqual(7, 7);
+assertEqual(12, 15);
