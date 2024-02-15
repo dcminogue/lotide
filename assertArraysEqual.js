c2bf8a706917622a3e3ee10f5203376001875a29
Daniel Minogue <danielcminogue@gmail.com>
2024-02-15 15:52:50 -0700

    Refactored assertArraysEqual.js code and created assertArraysEqualTest.js file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  let isEqual = [];
-  if (arr1.length !== arr2.length) {
-    return false;
-    
+const eqArrays = require(`./eqArrays`);
 
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        isEqual.push(true);
-      } else {
-        isEqual.push(false);
-      }
-    }
-  }
-  if (isEqual.includes(false)) {
-    return false;
-  } else {
-    return true;
-  }
-};
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -27,7 +8,5 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log("Nope, these arrays are not equal. 🔴");
   }
 };
-assertArraysEqual([1, 10, 50], [1, 10, 50]);
-assertArraysEqual(["Rainbows", "Sky"], ["Blueberry", "Pie"]);
-assertArraysEqual(["john"], ["John"]);
-assertArraysEqual(["Victory", 17], ["Victory", 17]);
+
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require(`../assertArraysEqual`);
+
+// TEST CODE
+
+assertArraysEqual([1, 10, 50], [1, 10, 50]);
+assertArraysEqual(["Rainbows", "Sky"], ["Blueberry", "Pie"]);
+assertArraysEqual(["john"], ["John"]);
+assertArraysEqual(["Victory", 17], ["Victory", 17]);
