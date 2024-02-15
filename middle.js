333ea53437ed54701b9b84ec8650e33c77b5786f
Daniel Minogue <danielcminogue@gmail.com>
2024-02-15 16:30:33 -0700

    refactored middle.js function

--- middle.js
+++ middle.js
@@ -1,18 +1,13 @@
 const middle = function(arr) {
-  const middleOfArray = [];
   if (arr.length <= 2) {
     return [];
-  } else {
+  } 
     if (arr.length % 2 !== 0) {
-      middleOfArray.push(arr[Math.floor(arr.length / 2)]);
-      return middleOfArray;
-    } else {
-    
-      middleOfArray.push(arr[arr.length / 2 - 1], arr[arr.length / 2]);
-      return middleOfArray;
+      return [arr[Math.floor(arr.length / 2)]];
+      } else {
+      return [arr[arr.length / 2 - 1], arr[arr.length / 2]];
     }
-  }
-};
+  };
 
 module.exports = middle;
 
